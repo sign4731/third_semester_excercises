28fa2c75a482f5930373389f6928da1b8b61b77a
Signe <sign4731@stud.kea.dk>
2021-02-09 09:21:44 +0100

    mix

---
+++ capitalization.js
@@ -0,0 +1,8 @@
+"use strict";
+
+const name1 = "peterparker";
+
+const firstLetter = name1.substring(0, 1).toUpperCase();
+const restOfLetters = name1.substring(1);
+
+console.log(`${firstLetter}${restOfLetters}`);
