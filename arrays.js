60ad4ee2c0b905963e328f997a19333cdbc313d7
Signe <sign4731@stud.kea.dk>
2021-02-11 12:53:01 +0100

    loop

--- arrays.js
+++ arrays.js
@@ -16,20 +16,20 @@ let result;
 // result = people.pop();
 // result = people.push("Neville");
 // result = people.push("Luna");
-// result = people.slice(0, 3);
+result = people.slice(0, 3);
 // result = people.slice(1, 0, "Cho");
 // people[1] = "Ginny";
 // result = people.push("Fred", "George");
 // result = people.indexOf("Fred");
 // result = people.splice(result, 1);
 // result = people.unshift("Wilfred");
-result = people.shift();
+// result = people.shift();
 
 console.log(result);
 console.log(people);
 
-const str = "abcdefghijklmn";
-// const arr1 = str.split();
-const arr2 = Array.from(str);
-const str2 = arr2.toString();
-console.log(str2);
+// const str = "abcdefghijklmn";
+// // const arr1 = str.split();
+// const arr2 = Array.from(str);
+// const str2 = arr2.toString();
+// console.log(str2);
---
+++ loop.js
@@ -0,0 +1,30 @@
+"use strict";
+
+// for (let counter = 0; counter < 10; counter++) {
+//   console.log(counter);
+// }
+
+// for (let counter = 1; counter <= 10; counter++) {
+//   console.log(counter);
+// }
+
+// for (let counter = 10; counter > -1; counter--) {
+//   console.log(counter);
+// }
+// console.log("liftoff");
+
+// for (let counter = 1; counter < 19; counter += 2) {
+//   console.log(counter);
+// }
+
+// for (let counter = 1; counter <= 16777216; counter *= 2) {
+//   console.log(counter);
+// }
+
+// for (let counter = 111; counter <= 138; counter += 3) {
+//   console.log(counter);
+// }
+
+for (let counter = 100; counter > -1; counter -= 10) {
+  console.log(counter);
+}
